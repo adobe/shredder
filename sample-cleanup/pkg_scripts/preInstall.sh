30c5f7b6f7d05936981d317c5bb94c64ab9bc11a
Alexandru Popa <apopa@adobe.com>
2019-02-15 17:29:28 +0200

    Fix: Remove custom user and group creation from the preinstall scripts (fixes: #1)

--- sample-cleanup/pkg_scripts/preInstall.sh
+++ sample-cleanup/pkg_scripts/preInstall.sh
@@ -1,2 +1,6 @@
+getent group ddx >/dev/null || groupadd -r ddx
+getent passwd ddxapp >/dev/null || useradd -r -g ddx -s /bin/bash -m -d /home/ddxapp -c "Ddxapp user" ddxapp
+
 mkdir -p /var/run/demdex
+chown ddxapp:ddx /var/run/demdex
 exit 0
