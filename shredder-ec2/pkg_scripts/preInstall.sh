1531c299c2ebdd758db09850dde1d8f2c9c20190
Alexandru Popa <alexpopero@users.noreply.github.com>
2019-02-15 17:25:37 +0200

    Revert "Fix: Remove custom user and group creation from the preinstall scripts (fixes: #1)"
    This reverts commit 5c6c9e1010b358f4af671c8c4ae345b8d083c6a1.

--- shredder-ec2/pkg_scripts/preInstall.sh
+++ shredder-ec2/pkg_scripts/preInstall.sh
@@ -1,2 +1,6 @@
+getent group ddx >/dev/null || groupadd -g 5000 -r ddx
+getent passwd ddxapp >/dev/null || useradd -r -u 1001 -g ddx -s /bin/bash -m -d /home/ddxapp -c "Ddxapp user" ddxapp
+
 mkdir -p /var/run/demdex
+chown ddxapp:ddx /var/run/demdex
 exit 0
